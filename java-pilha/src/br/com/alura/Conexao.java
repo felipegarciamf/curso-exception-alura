8355a0c00c7b1ecc3128b7bb8e5dd0e2b857fa19
felipegarciamf <silasfelipegarcia@gmail.com>
2021-05-07 10:14:41 -0300

    criando formato simplificado sem finally para excecao

--- java-pilha/src/br/com/alura/Conexao.java
+++ java-pilha/src/br/com/alura/Conexao.java
@@ -1,19 +1,18 @@
 package br.com.alura;
 
-public class Conexao {
+public class Conexao implements AutoCloseable {
 
-	
 	public Conexao() {
 		System.out.println("Abrindo conexao!");
 	}
-	
+
 	public void leDados() {
 		System.out.println("Recebendo Dados");
 		throw new IllegalStateException();
 	}
-	
-	
-	public void fecha() {
+
+	@Override
+	public void close() {
 		System.out.println("Fechando conexao");
 	}
 }
--- java-pilha/src/br/com/alura/TesteConexao.java
+++ java-pilha/src/br/com/alura/TesteConexao.java
@@ -3,15 +3,24 @@ package br.com.alura;
 public class TesteConexao {
 
 	public static void main(String[] args) {
-		Conexao con = null;
-		try {
-			con = new Conexao();
+
+		try (Conexao con = new Conexao()) {
 			con.leDados();
 		} catch (IllegalStateException e) {
-			System.out.println(e);
-		} finally {
-			con.fecha();
+			System.out.println("Deu erro na conexao");
 		}
+
+		// -------------------------------
+
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch (IllegalStateException e) {
+//			System.out.println(e);
+//		} finally {
+//			con.fecha();
+//		}
 
 	}
 
